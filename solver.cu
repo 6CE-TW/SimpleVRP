67d6f587860c1c78f6d5a4d83660444389086005
6CE-TW <xxxxx876216@yahoo.com.tw>
2025-05-14 00:08:40 +0900

    [feature]: add solver class and naive algorithm for initial solution

--- solver.cu
+++ solver.cu
@@ -1,13 +1,70 @@
+#include <iostream>
+#include <set>
 #include "solver.h"
 
-class SimpleVRPSolver
+void SimpleVRPSolver::Solve()
 {
-private:
-  std::vector<std::vector<double>> _cost_matrix;
-
-public:
-  SimpleVRPSolver(std::vector<std::vector<double>> cost_matrix)
+  if (this->initial_solution_strategy == InitialSolutionStrategy::NaiveInitialSolution)
   {
-    this->_cost_matrix = cost_matrix;
+    this->InitialSolutionNaive();
   }
-};
+}
+
+void SimpleVRPSolver::InitialSolutionNaive()
+{
+  std::set<std::size_t> traversed_nodes;
+  std::size_t latest_arrival_node = 0;
+
+  for (std::size_t i = 1; i < this->_num_of_nodes - 1; ++i)
+  {
+    std::size_t best_idx = 0;
+    std::size_t minimal_cost = INT32_MAX;
+    for (std::size_t j = 1; j < this->_num_of_nodes - 1; ++j)
+    {
+      if (traversed_nodes.find(j) != traversed_nodes.end())
+      {
+        continue;
+      }
+
+      if (this->_cost_matrix[latest_arrival_node][j] < minimal_cost)
+      {
+        best_idx = j;
+        minimal_cost = _cost_matrix[latest_arrival_node][j];
+      }
+    }
+
+    Route temp_route{latest_arrival_node, best_idx, 0};
+    this->route_records[0].push_back(temp_route);
+    this->_cost += minimal_cost;
+
+    traversed_nodes.insert(best_idx);
+    latest_arrival_node = best_idx;
+  }
+  Route temp_route{latest_arrival_node, this->_num_of_nodes - 1, 0};
+  this->route_records[0].push_back(temp_route);
+  this->_cost += this->_cost_matrix[latest_arrival_node][this->_num_of_nodes - 1];
+
+  for (std::size_t i = 1; i < this->_num_of_vehicles; ++i)
+  {
+    Route temp_route{0, this->_num_of_nodes - 1, i};
+    this->route_records[i].push_back(temp_route);
+    this->_cost += this->_cost_matrix[0][this->_num_of_nodes - 1];
+  }
+}
+
+void SimpleVRPSolver::PrintSolution()
+{
+  for (std::size_t i = 0; i < this->_num_of_vehicles; ++i)
+  {
+    std::size_t route_length = route_records.at(i).size();
+
+    std::cout << "vehicle " << i << "\n";
+    for (std::size_t j = 0; j < route_length; ++j)
+    {
+      const Route route = route_records.at(i).at(j);
+      std::cout << "(" << route.prev << ", " << route.next << ") ";
+    }
+    std::cout << "\n\n";
+  }
+  std::cout << "cost: " << this->_cost << "\n";
+}
--- solver.h
+++ solver.h
@@ -1,6 +1,43 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
-#include<vector>
+#include <vector>
+
+enum InitialSolutionStrategy
+{
+  NaiveInitialSolution = 0,
+};
+
+struct Route
+{
+  std::size_t prev;
+  std::size_t next;
+  std::size_t vehicle;
+};
+
+class SimpleVRPSolver
+{
+private:
+  std::vector<std::vector<double>> _cost_matrix;
+  std::size_t _num_of_vehicles;
+  std::size_t _num_of_nodes;
+  double _cost;
+  InitialSolutionStrategy initial_solution_strategy = InitialSolutionStrategy::NaiveInitialSolution;
+  std::vector<std::vector<Route>> route_records;
+
+  void InitialSolutionNaive();
+
+public:
+  SimpleVRPSolver(std::vector<std::vector<double>> cost_matrix, std::size_t num_of_vehicles)
+  {
+    this->_cost_matrix = cost_matrix;
+    this->_num_of_vehicles = num_of_vehicles;
+    this->_num_of_nodes = cost_matrix.size();
+    this->route_records = std::vector<std::vector<Route>>(num_of_vehicles, std::vector<Route>{});
+  }
+
+  void Solve();
+  void PrintSolution();
+};
 
 #endif // SOLVER_H
