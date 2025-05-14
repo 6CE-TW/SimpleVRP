b1b32eb7ae0c3b8705fb0fe54e8afd491f4546be
6CE-TW <xxxxx876216@yahoo.com.tw>
2025-05-15 00:53:31 +0900

    [file]: modify build.bat to fit the new structure

--- build.bat
+++ build.bat
@@ -1,7 +1,12 @@
 @echo off
 set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.8
+
+set COMPILER="%CUDA_PATH%\bin\nvcc.exe"
+set FLAGS= -I. -std=c++17
+
 set OUTPUT_PATH=output
+set SOURCE_FILE=main.cc data.cc solver.cc initial_solution/initial_solution.cc
 
 if not exist %OUTPUT_PATH% mkdir %OUTPUT_PATH%
 echo Compiling...
-"%CUDA_PATH%\bin\nvcc.exe" main.cc data.cc solver.cc -std=c++17 -o %OUTPUT_PATH%\main.exe
+%COMPILER% %FLAGS% %SOURCE_FILE% -o %OUTPUT_PATH%\main.exe
