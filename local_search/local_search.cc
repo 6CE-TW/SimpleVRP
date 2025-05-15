25955bd892301e5624284d5a4a9d65ddbfa39454
6CE-TW <xxxxx876216@yahoo.com.tw>
2025-05-15 22:17:14 +0900

    [feature]: add local search related codes

---
+++ local_search/local_search.cc
@@ -0,0 +1,37 @@
+#include "local_search/local_search.h"
+
+std::vector<LocalSearch> LocalSearchGenerator::GenerateLocalSearchList()
+{
+  // 0: TWO_OPT
+  if (this->usable_local_search[0] == true)
+  {
+  }
+  // 1: OR_OPT
+  if (this->usable_local_search[1] == true)
+  {
+  }
+  // 2: RELOCATE
+  if (this->usable_local_search[2] == true)
+  {
+  }
+  // 3: SWAP
+  if (this->usable_local_search[3] == true)
+  {
+  }
+  // 4: CROSS_EXCHANGE
+  if (this->usable_local_search[4] == true)
+  {
+  }
+  // 5: THREE_OPT
+  if (this->usable_local_search[5] == true)
+  {
+  }
+  // 6: LAMBDA_EXCHANGE
+  if (this->usable_local_search[6] == true)
+  {
+  }
+  // 7: LNS
+  if (this->usable_local_search[7] == true)
+  {
+  }
+}
---
+++ local_search/local_search.h
@@ -0,0 +1,63 @@
+#ifndef LOCAL_SEARCH_H
+#define LOCAL_SEARCH_H
+
+#include <iostream>
+#include <vector>
+
+enum LocalSearchEnum
+{
+  TWO_OPT = 0,
+  OR_OPT = 1,
+  RELOCATE = 2,
+  SWAP = 3,
+
+  CROSS_EXCHANGE = 4,
+  THREE_OPT = 5,
+  LAMBDA_EXCHANGE = 6,
+
+  LNS = 7,
+};
+
+class LocalSearch
+{
+public:
+  size_t vehicle;
+};
+
+class TwoOpt : public LocalSearch
+{
+public:
+  size_t path_start;
+  size_t path_end;
+
+  void Print()
+  {
+    std::cout << "TwoOpt - Vehicle: " << this->vehicle
+              << " Reverse Path: (" << this->path_start << " -> " << this->path_end << ")\n";
+  }
+};
+
+class LocalSearchGenerator
+{
+private:
+  std::vector<bool> usable_local_search = {
+      true, // 0: TWO_OPT
+      true, // 1: OR_OPT
+      true, // 2: RELOCATE
+      true, // 3: SWAP
+      true, // 4: CROSS_EXCHANGE
+      true, // 5: THREE_OPT
+      true, // 6: LAMBDA_EXCHANGE
+      true, // 7: LNS
+  };
+  std::vector<std::vector<std::size_t>> _node_records;
+
+public:
+  LocalSearchGenerator(std::vector<std::vector<std::size_t>> node_records)
+  {
+    this->_node_records = node_records;
+  }
+  std::vector<LocalSearch> GenerateLocalSearchList();
+};
+
+#endif // LOCAL_SEARCH_H
