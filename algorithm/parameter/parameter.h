e65f50869ac10b284baed3f76eedf50c9fc6dd3c
6CE-TW <xxxxx876216@yahoo.com.tw>
2025-05-22 02:16:50 +0900

    [feature]: modify the getter and setter of the local search parameter

--- algorithm/parameter/parameter.h
+++ algorithm/parameter/parameter.h
@@ -7,7 +7,7 @@
 class LocalSearchParameter
 {
 private:
-  std::unordered_map<LocalSearchEnum, bool> rearranged_local_search = {
+  std::unordered_map<LocalSearchEnum, bool> _rearranged_local_search = {
       {LocalSearchEnum::TWO_OPT, true},
       {LocalSearchEnum::OR_OPT, true},
       {LocalSearchEnum::THREE_OPT, true},
@@ -19,33 +19,54 @@ private:
   };
 
 public:
-  LocalSearchParameter(){};
+  LocalSearchParameter() {};
   LocalSearchParameter(std::unordered_map<LocalSearchEnum, bool> usable_local_search);
 
-  std::unordered_map<LocalSearchEnum, bool> GetRearrangedLocalSearchList()
+  std::unordered_map<LocalSearchEnum, bool> rearranged_local_search()
   {
-    return this->rearranged_local_search;
+    return this->_rearranged_local_search;
+  }
+
+  void set_rearranged_local_search(const std::unordered_map<LocalSearchEnum, bool> usable_local_search)
+  {
+    this->_rearranged_local_search = usable_local_search;
+    if (this->_rearranged_local_search[LocalSearchEnum::THREE_OPT])
+    {
+      this->_rearranged_local_search[LocalSearchEnum::TWO_OPT] = true;
+      // Here we assume OR_OPT_LENGTH_LIMIT_IN_THREE_OPT >= OR_OPT_LENGTH_LIMIT
+      // TODO: Consider the situation that length limit in OR OPT is larger
+      this->_rearranged_local_search[LocalSearchEnum::OR_OPT] = false;
+      this->_rearranged_local_search[LocalSearchEnum::RELOCATE_SAME_VEHICLE] = false;
+    }
+    if (this->_rearranged_local_search[LocalSearchEnum::OR_OPT])
+    {
+      this->_rearranged_local_search[LocalSearchEnum::RELOCATE_SAME_VEHICLE] = false;
+    }
+    if (this->_rearranged_local_search[LocalSearchEnum::CROSS])
+    {
+      this->_rearranged_local_search[LocalSearchEnum::EXCHANGE] = false;
+    }
   }
 };
 
 LocalSearchParameter::LocalSearchParameter(std::unordered_map<LocalSearchEnum, bool> usable_local_search)
 {
-  this->rearranged_local_search = usable_local_search;
-  if(this->rearranged_local_search[LocalSearchEnum::THREE_OPT])
+  this->_rearranged_local_search = usable_local_search;
+  if (this->_rearranged_local_search[LocalSearchEnum::THREE_OPT])
   {
-    this->rearranged_local_search[LocalSearchEnum::TWO_OPT] = true;
+    this->_rearranged_local_search[LocalSearchEnum::TWO_OPT] = true;
     // Here we assume OR_OPT_LENGTH_LIMIT_IN_THREE_OPT >= OR_OPT_LENGTH_LIMIT
     // TODO: Consider the situation that length limit in OR OPT is larger
-    this->rearranged_local_search[LocalSearchEnum::OR_OPT] = false;
-    this->rearranged_local_search[LocalSearchEnum::RELOCATE_SAME_VEHICLE] = false;
+    this->_rearranged_local_search[LocalSearchEnum::OR_OPT] = false;
+    this->_rearranged_local_search[LocalSearchEnum::RELOCATE_SAME_VEHICLE] = false;
   }
-  if(this->rearranged_local_search[LocalSearchEnum::OR_OPT])
+  if (this->_rearranged_local_search[LocalSearchEnum::OR_OPT])
   {
-    this->rearranged_local_search[LocalSearchEnum::RELOCATE_SAME_VEHICLE] = false;
+    this->_rearranged_local_search[LocalSearchEnum::RELOCATE_SAME_VEHICLE] = false;
   }
-  if(this->rearranged_local_search[LocalSearchEnum::CROSS])
+  if (this->_rearranged_local_search[LocalSearchEnum::CROSS])
   {
-    this->rearranged_local_search[LocalSearchEnum::EXCHANGE] = false;
+    this->_rearranged_local_search[LocalSearchEnum::EXCHANGE] = false;
   }
 }
 
