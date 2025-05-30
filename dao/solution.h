65c81125ff52de086107b89c6066fad5d9883eb7
6CE-TW <xxxxx876216@yahoo.com.tw>
2025-05-31 02:50:55 +0900

    [feature]: add function to output geojson

--- dao/solution.h
+++ dao/solution.h
@@ -1,5 +1,7 @@
 #include <string>
 #include <vector>
+#include <sstream>
+
 #include "dao/destination.h"
 
 #ifndef NLOHMANN_JSON_H_
@@ -125,6 +127,144 @@ public:
     data["calculate_info"]["version_name"] = this->version_name;
     data["calculate_info"]["version_number"] = this->version_number;
 
+    return data;
+  }
+
+  std::string NumToHexString(double number)
+  {
+    int rounded = std::clamp(static_cast<int>(std::round(number)), 0, 255);
+    std::ostringstream ss;
+    ss << std::uppercase << std::hex;
+    if (rounded < 16) ss << '0';
+    ss << rounded;
+    return ss.str();
+  }
+
+  std::vector<std::vector<std::string>> GenerateVehicleColor(int num_of_vehicle)
+  {
+    std::vector<std::vector<std::string>> output;
+
+    for (int i = 0; i < num_of_vehicle; ++i)
+    {
+      double hue = i * 360.0 / num_of_vehicle;
+      double saturation = 0.5;
+      double value = 1.0;
+
+      int hue_region = static_cast<int>(hue / 60.0) % 6;
+      double f = (hue / 60.0) - hue_region;
+
+      double p = value * (1 - saturation);
+      double q = value * (1 - f * saturation);
+      double t = value * (1 - (1 - f) * saturation);
+
+      double r = 0, g = 0, b = 0;
+
+      // clang-format off
+        switch (hue_region) {
+          case 0: r = value; g = t; b = p; break;
+          case 1: r = q; g = value; b = p; break;
+          case 2: r = p; g = value; b = t; break;
+          case 3: r = p; g = q; b = value; break;
+          case 4: r = t; g = p; b = value; break;
+          case 5: r = value; g = p; b = q; break;
+        }
+      // clang-format on
+
+      std::vector<std::string> color_hex = {
+          NumToHexString(r * 255),
+          NumToHexString(g * 255),
+          NumToHexString(b * 255)};
+
+      output.push_back(color_hex);
+    }
+
+    return output;
+  }
+
+  json ToGeoJson()
+  {
+    json data;
+    data["type"] = "FeatureCollection";
+
+    std::size_t num_of_vehicle = this->vehicle_task_routes.size();
+    std::vector<std::vector<std::string>> vehicle_colors = GenerateVehicleColor(num_of_vehicle);
+
+    json features_json;
+    for (std::size_t i = 0; i < num_of_vehicle; ++i)
+    {
+      const VehicleTaskRoute vehicle_task_route = vehicle_task_routes.at(i);
+      std::vector<std::string> vehicle_color = vehicle_colors.at(i);
+      std::string color_hex = vehicle_color.at(0) + vehicle_color.at(1) + vehicle_color.at(2);
+      json coordinates_list;
+      for (std::size_t j = 0; j < vehicle_task_route.tasks.size(); ++j)
+      {
+        /*
+        "type": "Feature",
+        "properties": {
+            "marker-color": "#0000F0",
+            "marker-size": "medium",
+            "marker-symbol": "",
+            "location": "wsqq5r98v"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                121.43418073654175,
+                25.002286434173584
+            ]
+        }
+        */
+        Destination destination = vehicle_task_route.tasks.at(j).destination;
+
+        json feature;
+        feature["type"] = "Feature";
+        feature["properties"]["marker-color"] = "#" + color_hex;
+        feature["properties"]["marker-size"] = "medium";
+        feature["geometry"]["type"] = "Point";
+        feature["geometry"]["coordinates"] = {destination.lon, destination.lat};
+        features_json.push_back(feature);
+
+        coordinates_list.push_back({destination.lon, destination.lat});
+      }
+
+      /*
+      "type": "Feature",
+      "properties": {
+          "strokeColor": "#0000F0"
+      },
+      "style": {
+          "fill": "#0000F0"
+      },
+      "geometry": {
+          "type": "LineString",
+          "coordinates": [
+              [
+                  121.43418073654175,
+                  25.002286434173584
+              ],
+              [
+                  121.49666547775269,
+                  25.057432651519775
+              ],
+              ...
+              [
+                  121.43418073654175,
+                  25.002286434173584
+              ]
+          ]
+      }
+      */
+
+      json line_feature;
+      line_feature["type"] = "Feature";
+      line_feature["properties"]["strokeColor"] = "#" + color_hex;
+      line_feature["style"]["fill"] = "#" + color_hex;
+      line_feature["geometry"]["type"] = "LineString";
+      line_feature["geometry"]["coordinates"] = coordinates_list;
+      features_json.push_back(line_feature);
+    }
+
+    data["features"] = features_json;
     return data;
   }
 };
