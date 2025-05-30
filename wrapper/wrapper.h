90434fd95582b5d2e94406d1dd49249a95035f60
6CE-TW <xxxxx876216@yahoo.com.tw>
2025-05-31 01:40:07 +0900

    [fix]: fix the compile error of the wrapper function

--- wrapper/wrapper.h
+++ wrapper/wrapper.h
@@ -18,17 +18,7 @@ struct Parameter
   std::vector<Destination> destinations;
 };
 
-std::string DumpParameter(Parameter parameter)
-{
-  json data;
-
-  std::vector<json> destinations(parameter.destinations.size());
-  std::transform(parameter.destinations.begin(), parameter.destinations.end(), destinations.begin(), [](Destination d)
-                 { return d.ToJson(); });
-  data["destinations"] = destinations;
-
-  return data.dump();
-};
+std::string DumpParameter(Parameter parameter);
 
 class ParameterWrapper
 {
@@ -40,56 +30,8 @@ private:
   ParameterWrapper() {}
 };
 
-Parameter ParameterWrapper::FromJson(std::string str)
-{
-  return ParameterWrapper::FromJson(json::parse(str));
-};
+json ParameterToJsonDict(const Parameter &param);
 
-Parameter ParameterWrapper::FromJson(nlohmann::json json_obj)
-{
-  auto parameter = Parameter();
-
-  // #region destinations
-  if (json_obj.contains("destinations"))
-  {
-    auto json_destinations = json_obj.at("destinations");
-
-    for (std::size_t i = 0; i < json_destinations.size(); ++i)
-    {
-      auto elem = json_destinations.at(i);
-
-      auto destination = Destination();
-      destination.name = elem.value("name", "");
-      destination.lon = elem.at("lon").get<std::double_t>();
-      destination.lat = elem.at("lat").get<std::double_t>();
-
-      // append into struct
-      parameter.destinations.push_back(destination);
-    }
-  }
-  // #endregion destinations
-
-  return parameter;
-};
-
-json ParameterToJsonDict(const Parameter &param)
-{
-  json j_array = json::array();
-  for (const auto &dest : param.destinations)
-  {
-    j_array.push_back(dest.ToJson());
-  }
-  return j_array;
-}
-
-json ParameterToJsonList(const Parameter &param)
-{
-  json j_array = json::array();
-  for (const auto &dest : param.destinations)
-  {
-    j_array.push_back({dest.lon, dest.lat});
-  }
-  return j_array;
-}
+json ParameterToJsonList(const Parameter &param);
 
 #endif
